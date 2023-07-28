29ec79d26d996dabf1ef1a5176c4d89831e1c7a0
danebidev <57720273+danebidev@users.noreply.github.com>
2023-07-28 12:06:51 +0000

    Started configuring nvim

---
+++ config/nvim/init.lua
@@ -0,0 +1,42 @@
+-- Requires
+
+require "user.settings"
+
+
+-- Variables
+
+local home = vim.env.HOME
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local opt = vim.opt
+
+
+-- Bootstrap lazy.nvim
+
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Plugins
+
+require("lazy").setup({
+    -- Colorscheme
+	{
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+	        vim.cmd.colorscheme("tokyonight")
+    	end
+    },
+
+})
+
---
+++ config/nvim/lua/user/settings.lua
@@ -0,0 +1,49 @@
+local options = {
+
+    ----------------------------- Indentation ----------------------------
+    tabstop = 4,
+    shiftwidth = 4,
+    shiftround = true,
+    autoindent = true,
+    smartindent = true,
+    ----------------------------------------------------------------------
+
+    ------------------------- Movement and search ------------------------
+    wrapscan = true,
+    incsearch = true,
+    ignorecase = true,
+    scrolloff = 25,
+    sidescrolloff = 5,
+    ----------------------------------------------------------------------
+
+    -------------------------------- Text --------------------------------
+    wrap = false,
+    linebreak = true,
+    breakindent = true,
+    formatoptions = "nqc1jp",
+    ----------------------------------------------------------------------
+    
+    --------------------------------- UI ---------------------------------
+    shortmess = "filnxtToOFcsS",
+    number = true,
+    relativenumber = true,
+    termguicolors = true,
+    signcolumn = "yes",
+    foldlevelstart = 99,
+    ----------------------------------------------------------------------
+
+    ------------------------------- Extra --------------------------------
+    mouse = "nv",
+    splitbelow = true,
+    splitright = true,
+    showmode = false,
+    completeopt = { "menu", "menuone", "noinsert", "noselect" },
+    swapfile = false,
+    undofile = true
+    ----------------------------------------------------------------------
+
+}
+
+for k,v in pairs(options) do
+    vim.opt[k] = v
+end
