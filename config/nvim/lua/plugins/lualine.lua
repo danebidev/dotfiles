99fa7b94a6423cf5a84883573040871ee646ee3e
danebidev <57720273+danebidev@users.noreply.github.com>
2023-08-29 00:11:49 +0200

    Changed lualine components

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,41 +1,42 @@
 local lualine = require("lualine")
 
-local function mixed_indent()
-	local space_pat = [[\v^ +]]
-	local tab_pat = [[\v^\t+]]
-	local space_indent = vim.fn.search(space_pat, "nwc")
-	local tab_indent = vim.fn.search(tab_pat, "nwc")
-	local mixed = (space_indent > 0 and tab_indent > 0)
-	local mixed_same_line
-	if not mixed then
-		mixed_same_line = vim.fn.search([[\v^(\t+ | +\t)]], "nwc")
-		mixed = mixed_same_line > 0
-	end
-	if not mixed then
-		return ""
-	end
-	if mixed_same_line ~= nil and mixed_same_line > 0 then
-		return "MI:" .. mixed_same_line
-	end
-	local space_indent_cnt = vim.fn.searchcount({ pattern = space_pat, max_count = 1e3 }).total
-	local tab_indent_cnt = vim.fn.searchcount({ pattern = tab_pat, max_count = 1e3 }).total
-	if space_indent_cnt > tab_indent_cnt then
-		return "MI:" .. tab_indent
-	else
-		return "MI:" .. space_indent
-	end
-end
+local mode = {
+	"mode",
+	fmt = function(str)
+		return " " .. str
+	end,
+}
+
+local filename = {
+	"filename",
+}
+
+local progress = {
+	"progress",
+	fmt = function(str)
+		if str == "Top" then
+			return " 0%%"
+		end
+		if str == "Bot" then
+			return "100%%"
+		end
+		return str
+	end,
+}
 
 lualine.setup({
 	options = {
 		theme = "auto",
-		section_separators = "",
+		section_separators = { left = "", right = "" },
 		component_separators = "",
 		refresh = { statusline = 300 },
 	},
 	sections = {
-		lualine_c = {
-			"lsp_progress",
-		},
+		lualine_a = { mode },
+		lualine_b = { "branch", "diff" },
+		lualine_c = { filename, "diagnostics" },
+		lualine_x = { lsp_progress },
+		lualine_y = { "filetype", "encoding" },
+		lualine_z = { progress },
 	},
 })
