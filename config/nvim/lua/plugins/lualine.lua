a1f167d5b681787cdafef577c752adcd8d0410ed
danebidev <57720273+danebidev@users.noreply.github.com>
2023-08-29 17:47:13 +0200

    Finished configuring lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -9,10 +9,25 @@ local mode = {
 
 local filename = {
 	"filename",
+	newfile_status = true,
+	symbols = { modified = "󰏫 ", readonly = "󰏮 " },
+}
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	symbols = { error = " ", warn = " ", info = " ", hint = " " },
+}
+
+local lsp_progress = {
+	"lsp_progress",
+	display_components = { "spinner", { "percentage" }, "lsp_client_name" },
+	timer = { spinner = 200 },
 }
 
 local progress = {
 	"progress",
+	-- separator = { left = "", right = "" },
 	fmt = function(str)
 		if str == "Top" then
 			return " 0%%"
@@ -27,16 +42,16 @@ local progress = {
 lualine.setup({
 	options = {
 		theme = "auto",
-		section_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
 		component_separators = "",
-		refresh = { statusline = 300 },
+		refresh = { statusline = 100 },
 	},
 	sections = {
 		lualine_a = { mode },
 		lualine_b = { "branch", "diff" },
-		lualine_c = { filename, "diagnostics" },
+		lualine_c = { "filetype", filename, diagnostics },
 		lualine_x = { lsp_progress },
-		lualine_y = { "filetype", "encoding" },
+		lualine_y = { "encoding", "fileformat" },
 		lualine_z = { progress },
 	},
 })
