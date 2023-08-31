7c8ae6f8721736c079cd1c8e6bc0fdbf4f8b1edb
danebidev <57720273+danebidev@users.noreply.github.com>
2023-08-31 15:58:10 +0200

    Configured neo-tree

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -5,5 +5,15 @@ neotree.setup({
 		statusline = true,
 		tabs_layout = "equal",
 	},
+	filesystem = {
+		filtered_items = {
+			visible = true,
+			hide_dotfiles = false,
+			hide_by_name = {
+				".git"
+			}
+		},
+		hijack_netrw_behavior = "open_default"
+	},
 	close_if_last_window = true,
 })
