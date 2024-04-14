53fbc32eb6a98ca03e1d13268cff0a717fb34a05
danebidev <57720273+danebidev@users.noreply.github.com>
2024-04-15 00:02:31 +0200

    Changes

--- conf.sh
+++ conf.sh
@@ -6,28 +6,6 @@ if [ "$response" != "y" ]; then
     exit 1
 fi
 
-mkdir ~/.config
-
-for folder in $(ls ./config); do
-    rm -rf ~/.config/$folder
-done
-
-for folder in $(ls ./config); do
-    ln -s $(pwd)/config/$folder ~/.config/$folder
-done
-
-for folder in $(ls ./etc); do
-    sudo rm -rf /etc/$folder
-done
-
-for folder in $(ls ./etc); do
-    sudo cp -r $(pwd)/etc/$folder /etc/$folder
-done
-
-sudo pacman -Syu
-
-sudo pacman -S --needed git base-devel
-
 if ! pacman -Qs yay > /dev/null; then
     git clone https://aur.archlinux.org/yay.git
     cd yay
@@ -36,9 +14,28 @@ if ! pacman -Qs yay > /dev/null; then
     rm -rf yay
 fi
 
+mkdir ~/.config
+
+for folder in $(ls ./config); do
+    rm -rf ~/.config/$folder
+    ln -s $(pwd)/config/$folder ~/.config/$folder
+done
+
+for folder in $(ls ./etc); do
+    sudo rm -rf /etc/$folder
+    sudo cp -r $(pwd)/etc/$folder /etc/$folder
+done
+
+sudo pacman -Syu
+sudo pacman -S --needed git base-devel
+
 yay -S --needed $(cat ./pkglist)
 
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+rm ~/.zshrc
+rm ~/.zshenv
+rm ~/.xinitrc
 
 ln -s $(pwd)/home/zshrc ~/.zshrc
 ln -s $(pwd)/home/zshenv ~/.zshenv
