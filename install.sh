40c839a77ec185c70636feb757055a5ee5678b9e
danebidev <57720273+danebidev@users.noreply.github.com>
2024-06-12 02:38:16 +0200

    Switched to stow to manage dotfiles

--- install.sh
+++ install.sh
@@ -6,37 +6,23 @@ if [ "$response" != "y" ]; then
     exit 1
 fi
 
+sudo pacman -Syu
+sudo pacman -S --needed git base-devel wget stow
+
 if ! pacman -Qs yay > /dev/null; then
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
     cd ..
     rm -rf yay
+    yay -Y --gendb
+    yay -Y --save --devel --answerclean A --answerdiff N --answeredit N --redownload
 fi
 
 mkdir ~/.config
-
-for folder in $(ls ./config); do
-    rm -rf ~/.config/$folder
-    ln -s $(pwd)/config/$folder ~/.config/$folder
-done
-
-for folder in $(ls ./etc); do
-    sudo rm -rf /etc/$folder
-    sudo cp -r $(pwd)/etc/$folder /etc/$folder
-done
-
-sudo pacman -Syu
-sudo pacman -S --needed git base-devel
 
 yay -S --needed $(cat ./pkglist)
 
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-rm ~/.zshrc
-rm ~/.zshenv
-rm ~/.xinitrc
-
-ln -s $(pwd)/home/zshrc ~/.zshrc
-ln -s $(pwd)/home/zshenv ~/.zshenv
-ln -s $(pwd)/home/xinitrc ~/.xinitrc
+stow --dotfiles -t ~ --adopt eww hypr nvim xorg zsh
