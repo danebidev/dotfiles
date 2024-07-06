1a9ef83dd45e57f755afb730ad238beb77e04874
danebidev <57720273+danebidev@users.noreply.github.com>
2024-07-06 13:18:54 +0200

    Removed copilot chat

--- nvim/dot-config/nvim/lua/plugins/editor/copilot.lua
+++ nvim/dot-config/nvim/lua/plugins/editor/copilot.lua
@@ -1,13 +1,13 @@
 return {
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
-        dependencies = {
-            "nvim-lua/plenary.nvim"
-        },
-        opts = {
-        },
-    },
+    -- {
+    --     "CopilotC-Nvim/CopilotChat.nvim",
+    --     branch = "canary",
+    --     dependencies = {
+    --         "nvim-lua/plenary.nvim"
+    --     },
+    --     opts = {
+    --     },
+    -- },
     {
         "github/copilot.vim",
     }
