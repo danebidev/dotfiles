e936b9fdc0741fca0db76893a6d23e8a15ac70aa
danebidev <57720273+danebidev@users.noreply.github.com>
2024-08-11 16:16:41 +0200

    [sway] Fixed screenshot script

--- config/sway/scripts/screenshot.sh
+++ config/sway/scripts/screenshot.sh
@@ -3,5 +3,5 @@
 hyprpicker -rz &
 sleep 0.2
 grim  -g "$(slurp -d)" - | wl-copy && pkill hyprpicker
-mkdir -p ~/pictures/screenshot
+mkdir -p ~/pictures/screenshots
 wl-paste > ~/pictures/screenshots/screenshot_$(date -u +%Y%m%d-%H%M%S).png
