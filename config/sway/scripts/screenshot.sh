4ff2c87ce3dabd2d4296a2f2d8f4fa4af9e6fb66
danebidev <57720273+danebidev@users.noreply.github.com>
2024-12-03 21:58:11 +0100

    [sway] Modified screenshot script to include swappy

--- config/sway/scripts/screenshot.sh
+++ config/sway/scripts/screenshot.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+mkdir -p ~/pictures/screenshots
 hyprpicker -rz &
 sleep 0.2
-grim  -g "$(slurp -d)" - | wl-copy && pkill hyprpicker
-mkdir -p ~/pictures/screenshots
+
+if [ $1 = "swappy" ]; then
+    grim -g "$(slurp -d)" - | { pkill hyprpicker; swappy -f - -o - ; } | wl-copy
+else
+    grim -g "$(slurp -d)" - | wl-copy
+    pkill hyprpicker
+fi
+
 wl-paste > ~/pictures/screenshots/screenshot_$(date -u +%Y%m%d-%H%M%S).png
